4ea50533085fe032be20d4b38ee5b1f00840fb05
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2023-02-07 12:40:20 +0500

    Default Props

--- React Fragment/App.js
+++ React Fragment/App.js
@@ -1,10 +1,22 @@
 import React from 'react'
-import Child from "./Child"
+// import Child from "./Child"
+
+// function App() {
+//   return (
+//     <>
+//       <Child />
+//     </>
+//   )
+// }
 
 function App() {
   return (
-    <>
-      <Child />
-    </>
+    <div>
+      <Card cardColor="red" height={200} width={400} />
+      <Card />
+      <Card cardColor="green" />
+    </div>
   )
 }
+
+export default App
