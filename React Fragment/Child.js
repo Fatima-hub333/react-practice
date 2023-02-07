c75e32dd734962554a1509e0e02517227f001951
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2023-02-07 11:25:03 +0500

    react.Fragment

---
+++ React Fragment/Child.js
@@ -0,0 +1,11 @@
+import React from "react"
+import Grandchild from "./Grandchild"
+
+function Child() {
+  return (
+    <>
+      <h1>I'm the Child component</h1>
+      <Grandchild />
+    </>
+  )
+}
