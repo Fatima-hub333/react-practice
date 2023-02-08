d55d991b68fbaf11afd36210c39fb65854c55caf
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2023-02-08 11:06:29 +0500

    Prop Types

--- React Fragment/Card.js
+++ React Fragment/Card.js
@@ -1,23 +1,30 @@
 import React from "react"
+import PropTypes from "prop-types"
 
-class Card extends React.Component {
-  static defaultProps = {
-    cardColor: "blue",
+function Card(props) {
+    const styles = {
+        backgroundColor: props.cardColor,
+        height: props.height,
+        width: props.width
+    }
+    
+    return (
+        <div style={styles}></div>
+    )
+}
+
+// Challenge: Add prop types for the height and width. Make at least one of them required.
+// Extra Challenge: Make it so your incoming cardColor is only valid if it is "blue" or "red".
+
+Card.propTypes = {
+    cardColor: PropTypes.oneOf(["red", "blue", "green", "purple"]).isRequired,
+    height: PropTypes.number.isRequired,
+    width: PropTypes.number
+}
+
+Card.defaultProps = {
     height: 100,
     width: 100
-  }
-  
-  render() {
-  const styles = {
-    backgroundColor: this.props.cardColor,
-    height: this.props.height,
-    width: this.props.width
-  }
-
-  return (
-    <div style={styles}></div>
-    )
-  }
 }
 
 export default Card
