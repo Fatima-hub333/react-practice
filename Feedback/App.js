2a4e8a5c82abf7c39c97126721949d617e0569a5
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2023-02-08 15:30:46 +0500

    Children: React children

--- Feedback/App.js
+++ Feedback/App.js
@@ -1,29 +1,43 @@
 import React from "react"
-import CTA from "./CTA"
+import InfoCallout from "./InfoCallout"
+import ImageCallout from "./ImageCallout"
+import EmailCallout from "./EmailCallout"
 
 function App() {
   return (
-    <div>
-      <CTA position="right">
-        <h1>This is an important CTA</h1>
-        <button>Click me now or you'll miss out!</button>
-      </CTA>
-
-      <CTA>
-        <form>
-          <input type="email" placeholder="Enter email address here" />
-          <br />
-          <button>Submit</button>
-        </form>
-      </CTA>
-
-      <CTA>
-        <form>
-          <input type="email" placeholder="Enter contact number here" />
-          <br />
-          <button>Submit</button>
-        </form>
-      </CTA>
-    </div>
+    <main>
+      <h1>Welcome</h1>
+      <InfoCallout
+        header="Don't miss out!"
+        body="Unless you don't suffer from FOMO, you better make sure you fill out the email form below "
+    </main>
   )
 }
+// import CTA from "./CTA"
+
+// function App() {
+//   return (
+//     <div>
+//       <CTA position="right">
+//         <h1>This is an important CTA</h1>
+//         <button>Click me now or you'll miss out!</button>
+//       </CTA>
+
+//       <CTA>
+//         <form>
+//           <input type="email" placeholder="Enter email address here" />
+//           <br />
+//           <button>Submit</button>
+//         </form>
+//       </CTA>
+
+//       <CTA>
+//         <form>
+//           <input type="email" placeholder="Enter contact number here" />
+//           <br />
+//           <button>Submit</button>
+//         </form>
+//       </CTA>
+//     </div>
+//   )
+// }
